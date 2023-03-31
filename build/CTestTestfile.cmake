72a20339b522ca4c1fdeafbb396552801e3f4d99
YixuanDu <duyixuan_seu@foxmail.com>
2023-03-31 17:00:01 +0800

    del build

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/zhacgsn/VSProjects/arxml-mapping-pcap
-# Build directory: /Users/zhacgsn/VSProjects/arxml-mapping-pcap/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(xmltest "/Users/zhacgsn/VSProjects/arxml-mapping-pcap/build/xmltest")
-set_tests_properties(xmltest PROPERTIES  PASS_REGULAR_EXPRESSION ", Fail 0" WORKING_DIRECTORY "/Users/zhacgsn/VSProjects/arxml-mapping-pcap" _BACKTRACE_TRIPLES "/Users/zhacgsn/VSProjects/arxml-mapping-pcap/CMakeLists.txt;50;add_test;/Users/zhacgsn/VSProjects/arxml-mapping-pcap/CMakeLists.txt;0;")
